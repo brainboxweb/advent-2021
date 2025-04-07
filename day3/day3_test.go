93b0ebe44adc5ed4a74bf6f3c88d36344515edeb
Gary Straughan <gary.straughan@brainboxweb.co.uk>
2025-04-07 13:15:34 +0100

    Day 3

---
+++ day3/day3_test.go
@@ -0,0 +1,87 @@
+package day3_test
+
+import (
+	"testing"
+
+	"github.com/brainboxweb/advent-2021/day3"
+	"github.com/brainboxweb/advent-2021/helpers"
+)
+
+func TestDay3(t *testing.T) {
+	dataSet := helpers.GetDataString("../data/day3.txt")
+	tests := []struct {
+		in       []string
+		expected int
+	}{
+		{
+			[]string{
+				"00100",
+				"11110",
+				"10110",
+				"10111",
+				"10101",
+				"01111",
+				"00111",
+				"11100",
+				"10000",
+				"11001",
+				"00010",
+				"01010",
+			},
+			198,
+		},
+		{
+			dataSet,
+			1092896,
+		},
+	}
+	for _, tt := range tests {
+		testname := "Day3"
+		t.Run(testname, func(t *testing.T) {
+			ans := day3.Part1(tt.in)
+			if ans != tt.expected {
+				t.Errorf("got %d, want %d", ans, tt.expected)
+			}
+		})
+	}
+}
+
+func TestDay3Part2(t *testing.T) {
+	dataSet := helpers.GetDataString("../data/day3.txt")
+	tests := []struct {
+		in       []string
+		expected int
+	}{
+		{
+			[]string{
+				"00100",
+				"11110",
+				"10110",
+				"10111",
+				"10101",
+				"01111",
+				"00111",
+				"11100",
+				"10000",
+				"11001",
+				"00010",
+				"01010",
+			},
+			230,
+		},
+		{
+
+			dataSet,
+			4672151,
+		},
+	}
+	for _, tt := range tests {
+		testname := "Day3"
+		t.Run(testname, func(t *testing.T) {
+			ans := day3.Part2(tt.in)
+			if ans != tt.expected {
+				t.Errorf("got %d, want %d", ans, tt.expected)
+			}
+		})
+	}
+}
